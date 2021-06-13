5aee555c321d361d7efffa5990c56320061bd589
42 Nesibe <nesibesahinrte2023@gmail.com>
2021-06-13 22:11:56 +0300

    models

--- MVC/README.md
+++ MVC/README.md
@@ -2,3 +2,6 @@
 1) Controller
     user.js && admin.js => controller -> products.js  //getProducts && getAddProduct && postAddProduct
     app.js => error.js                                // get404Page
+
+2) models
+    ürünlerin elle girilip kaydedilmesi ve tüm ürünlerin addproduct'ta görüntülenmesi 
--- MVC/controllers/products.js
+++ MVC/controllers/products.js
@@ -1,11 +1,14 @@
-const products=[
-    {name:'Samsumg',price:3000, image:'1.jpg', description:'iyi telefon'},
-    {name:'Huawai',price:4000, image:'2.jpg', description:'güzel telefon'},
-    {name:'IPhone',price:5000, image:'3.jpg', description:'çok iyi telefon'},
-    {name:'Sony',price:6000, image:'4.jpg', description:'mükemmel telefon'}
-];
+const Product=require('../models/product');
+
+// const products=[
+//     {name:'Samsumg',price:3000, image:'1.jpg', description:'iyi telefon'},
+//     {name:'Huawai',price:4000, image:'2.jpg', description:'güzel telefon'},
+//     {name:'IPhone',price:5000, image:'3.jpg', description:'çok iyi telefon'},
+//     {name:'Sony',price:6000, image:'4.jpg', description:'mükemmel telefon'}
+// ];
 
 exports.getProducts=(req, res, next) =>{
+    const products=Product.getAll();
     res.render('index',
     {
         title: 'Home Page',
@@ -23,14 +26,11 @@ exports.getAddProduct= (req, res, next) => {
 }
 
 exports.postAddProduct=(req,res,next)=>{
-    products.push(
-        {
-            name:req.body.name,
-            price:req.body.price,
-            image:req.body.image,
-            description:req.body.description
-        }
-    );
-    console.log(req.body)
+    const product=new Product(
+        req.body.name,
+        req.body.price,
+        req.body.imageUrl,
+        req.body.description );
+        product.saveProduct();
     res.redirect('/');
 }
